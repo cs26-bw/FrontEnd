48229c0b4c77ace10eec74526e0aaaded41f8e54
thejohnwatt <j.watt10@gmail.com>
2020-03-04 12:03:01 -0800

    users info displaying on screen

--- lambda_mud/src/game/RoomInfo.js
+++ lambda_mud/src/game/RoomInfo.js
@@ -5,26 +5,37 @@ import {UserContext} from "../contexts/UserContext"
 function RoomInfo() {
 
     const [loading, setLoading] = useState(false)
-    const { user, setUser } = useContext(UserContext)
-    console.log(user)
+    const [userInfo, setUserInfo] = useState(
+        {
+            name: '',
+            title: '',
+            description: ''
+        }
+    )
+    const [players, setPlayers] = useState()
+    console.log(`user:`, userInfo,' players:', players )
+    
 
-    // useEffect(() => {
-    //     setLoading(true)
-    //     axiosWithAuth()
-    //     .get('https://ferrari-mud.herokuapp.com/api/adv/init')
-    //     .then(res => {
-    //         setLoading(false)
-    //         console.log(res)
-    //     })
-    //     .catch(err => {
-    //         setLoading(false)
-    //     })
+    useEffect(() => {
+        setLoading(true)
+        axiosWithAuth()
+        .get('https://ferrari-mud.herokuapp.com/api/adv/init')
+        .then(res => {
+            setLoading(false)
+            setUserInfo({...userInfo, name: res.data.name, title: res.data.title, description: res.data.description})
+            setPlayers(res.data.players)
+        })
+        .catch(err => {
+            setLoading(false)
+        })
         
-    // },[])
+    },[])
 
     return (
         <div>
-
+            <h1>User: {userInfo.name}</h1>
+            <h3>Room: {userInfo.title}</h3>
+            <h3>Description: {userInfo.description}</h3>
         </div>
     )
 }
